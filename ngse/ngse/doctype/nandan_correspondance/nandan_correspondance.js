084c13d68ea0a985159e69fd91ff5dd2f223cc9d
Your Name <you@example.com>
2021-01-06 18:50:59 +0000

    custom docs

---
+++ ngse/ngse/doctype/nandan_correspondance/nandan_correspondance.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, Sambhaji Kolate and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Nandan Correspondance', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ ngse/ngse/doctype/nandan_correspondance/test_nandan_correspondance.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2021, Sambhaji Kolate and Contributors
+# See license.txt
+from __future__ import unicode_literals
+
+# import frappe
+import unittest
+
+class TestNandanCorrespondance(unittest.TestCase):
+	pass
